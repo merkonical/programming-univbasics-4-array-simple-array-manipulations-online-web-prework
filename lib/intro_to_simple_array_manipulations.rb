c5a37dc098e46b64d1cb317dc2fb81e465ed727c
Maverick Adolfo <merkonical@gmail.com>
2019-08-04 06:59:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,7 +38,7 @@ def using_flatten(instruments)
   instruments.flatten 
 end 
 
-def using_delete(instructors, "Steven")
-  instructors.delete("Steven")
+def using_delete(instructors, no_offense_steven)
+  instructors.delete(no_offense_steven)
 end 
 
